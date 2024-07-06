b0bd7a16d03da9c39860520e5ab0cd616b15f715
David <uzondudavid2004@gmail.com>
2024-07-06 21:51:38 +0100

    update .gitignore

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,14 @@
 import pg from 'pg';
 
+
+
 const pool = new pg.Pool({
-    user: process.env.USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB,
-    password: process.env.DB_PASSWORD,
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
     // @ts-ignore
-    port: process.env.DB_PORT
+    port: process.env.PGPORT
 });
 
 const client = await pool.connect();
